42ff02c29a2c740810e716c06972919e2f9976ba
SomkamolV <somkamolv@hotmail.com>
2025-04-01 16:54:56 +0700

    main => Add Column to user entity. Add user roles as Enum to Roles column.

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,2 +1,20 @@
+import { Roles } from 'src/utility/common/user-roles.enum';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
 export class UserEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column({type:'enum', enum:Roles,array:true, default:[Roles.USER]})
+  roles: Roles[];
 }
