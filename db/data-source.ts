bd156d73e9600d2e88ffa44aede532ff770e22d8
SomkamolV <somkamolv@hotmail.com>
2025-04-01 17:14:52 +0700

    main => add 'dist/**/*.entity{.ts,.js}' glob pattern to data source entities

--- db/data-source.ts
+++ db/data-source.ts
@@ -9,7 +9,7 @@ export const dataSourceOptions: DataSourceOptions = {
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  entities: [],
+  entities: ['dist/**/*.entity{.ts,.js}'],
   migrations: [],
   logging: false,
   synchronize: false,
