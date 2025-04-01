fdc0ce0431d0d587bfee3305f38225cab348702b
SomkamolV <somkamolv@hotmail.com>
2025-04-01 17:56:31 +0700

    main => data source add migration glob pattern. Add more command relates to migration.

--- db/data-source.ts
+++ db/data-source.ts
@@ -10,7 +10,7 @@ export const dataSourceOptions: DataSourceOptions = {
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
   entities: ['dist/**/*.entity{.ts,.js}'],
-  migrations: [],
+  migrations: ['dist/db/migrations/*{.ts,.js}'],
   logging: false,
   synchronize: false,
 };
