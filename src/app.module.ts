c1533980e37746cd050a22130fe25910003e3ab0
SomkamolV <somkamolv@hotmail.com>
2025-04-01 16:59:29 +0700

    main => app module -> Do not have controller and provider anymore. Users module import  typeorm module as forFeature and uses UserEntity.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { dataSourceOptions } from 'db/data-source';
 import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [TypeOrmModule.forRoot(dataSourceOptions), UsersModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
