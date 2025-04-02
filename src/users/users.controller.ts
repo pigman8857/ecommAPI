2aabced242bfbf491c42caa73f54341f08d0db3e
SomkamolV <somkamolv@hotmail.com>
2025-04-02 14:59:58 +0700

    main => implement signin with return accesstoken along with user data.

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -4,14 +4,22 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { UserSignUpDto } from './dto/user-signup.dto';
 import { UserEntity } from './entities/user.entity';
+import { UserSignInDto } from './dto/user-signin.dto';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post('signup')
-  async signup(@Body() userSignUp: UserSignUpDto): Promise<{user: UserEntity}>{
-    return {user: await this.usersService.signup(userSignUp)};
+  async signup(@Body() userSignUpDto: UserSignUpDto): Promise<{user: UserEntity}>{
+    return {user: await this.usersService.signup(userSignUpDto)};
+  }
+
+  @Post('signin')
+  async signin(@Body() userSignInDto: UserSignInDto): Promise<{user: UserEntity, accessToken: string}>{
+    const user = await this.usersService.signin(userSignInDto);
+    const accessToken = await this.usersService.accessToken(user);
+    return {accessToken , user};
   }
 
   @Post()
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -5,7 +5,11 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { UserEntity } from './entities/user.entity';
 import { UserSignUpDto } from './dto/user-signup.dto';
-import { hash } from 'bcrypt';
+import { hash,compare } from 'bcrypt';
+import { UserSignInDto } from './dto/user-signin.dto';
+import { sign } from 'jsonwebtoken';
+import { StringValue } from 'ms';
+
 @Injectable()
 export class UsersService {
   constructor(
@@ -24,6 +28,27 @@ export class UsersService {
     user = await this.usersRepository.save(user);
     delete user.password;
     return user;
+  }
+
+  async signin(userSignInDto: UserSignInDto): Promise<UserEntity>{
+    const {email, password} = userSignInDto;
+    const userExists = await this.usersRepository
+      .createQueryBuilder('users')
+      .addSelect('users.password')
+      .where('users.email=:email',{email})
+      .getOne();
+
+    if(!userExists)
+      throw new BadRequestException('Bad credentials.');
+
+    const matchPassword = await compare(password,userExists.password);
+
+    if(!matchPassword)
+      throw new BadRequestException('Bad credentials.');
+
+    delete userExists.password;
+
+    return userExists;
   }
 
   create(createUserDto: CreateUserDto) {
@@ -48,5 +73,13 @@ export class UsersService {
 
   async findUserByEmail(email: string): Promise<UserEntity> {
     return await this.usersRepository.findOneBy({email});
+  }
+
+  async accessToken(user: UserEntity): Promise<string> {
+    const expiresIn =  process.env.ACCESS_TOKEN_EXPIRE_TIME as StringValue;
+    return sign(
+      {id:user.id,email:user.email},
+      process.env.ACCESS_TOKEN_SECRET_KEY,
+      {expiresIn});
   }
 }
